dafbec83e463b88fc8667765a38ada908ce507cf
lhy <gkdbsx@naver.com>
2020-12-08 17:37:48 +0900

    mainForm design

--- src/main/webapp/blog-app/src/pages/MainForm.js
+++ src/main/webapp/blog-app/src/pages/MainForm.js
@@ -1,7 +1,7 @@
 // ok
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Container, Row, Col, Jumbotron, Tabs, Tab, SplitButton, ButtonGroup, Dropdown, DropdownButton, FormControl, Spinner } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron, Tabs, Tab, SplitButton, ButtonGroup, Dropdown, DropdownButton, FormControl, Spinner, Form, Button } from 'react-bootstrap';
 import TitleH3TagStyle from './constant/TitleH3TagStyle';
 import Br2 from './constant/Br2';
 
@@ -20,6 +20,12 @@ const JumbotronStyle=styled.div`
 `;
 
 const MainForm = () => {
+  const [search_locate, setSearch_locate] = useState('');
+
+  const inputHandle = (e) => {
+    setSearch_locate(e.target.value);
+  };
+
   useEffect(() => {
     // teamList fetch -> teamCard에 담아서 보여줌
     fetch("http://localhost:8000/teamList", {
@@ -97,7 +103,7 @@ const MainForm = () => {
                       key={1}
                       id={`dropdown-button-drop`}
                       size="sm"
-                      variant="outline-secondary"
+                      variant="outline-primary"
                       title="Select locate"
                       onSelect={(eventKey)=>{
                         setLocate(eventKey)
@@ -126,6 +132,18 @@ const MainForm = () => {
  
               <Jumbotron>
                 <TitleH3TagStyle msg="지금 가려고 하는 곳이 얼마나 안전한지 확인해 보세요"></TitleH3TagStyle>
+                <Br2/>
+
+                <Form inline>
+                  <Form.Control
+                    className="mr-sm-2"
+										type="text"
+										name="search_locate"
+										placeholder="Enter to locate"
+										onChange={inputHandle}
+										value={search_locate} />
+                  <Button onClick={function(){alert("complete. search about '" + search_locate + "'")}} variant="outline-primary">Search</Button>
+                </Form>
               </Jumbotron>
 
               <Jumbotron>
