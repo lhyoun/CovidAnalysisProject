c7b591a3e38854fd6fb0af14a9119405abfb28a0
narare0551 <naraekang20@gmail.com>
2020-12-09 14:57:13 +0900

    covidalarm

---
+++ src/main/java/com/project/MatchingPro/controller/CovidAlarmController.java
@@ -0,0 +1,38 @@
+package com.project.MatchingPro.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.MatchingPro.domain.CovidAlarm.CovidAlarm;
+import com.project.MatchingPro.domain.CovidAlarm.CovidAlarmRepository;
+import com.project.MatchingPro.domain.TotalData.TotalData;
+import com.project.MatchingPro.domain.TotalData.TotalDataRepository;
+import com.project.MatchingPro.domain.user.User;
+import com.project.MatchingPro.domain.user.UserRepository;
+import com.project.MatchingPro.service.TotalDataService;
+import com.project.MatchingPro.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class CovidAlarmController {
+
+	private final HttpSession session;
+	private final CovidAlarmRepository CovidAlarmService;
+	private final CovidAlarmRepository CovidAlarmRepository;
+	
+	@GetMapping("/CovidAlarm")
+	public List<CovidAlarm> allData() {
+		return CovidAlarmRepository.findAll();
+	}
+	
+}
+
---
+++ src/main/java/com/project/MatchingPro/domain/CovidAlarm/CovidAlarmRepository.java
@@ -0,0 +1,7 @@
+package com.project.MatchingPro.domain.CovidAlarm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CovidAlarmRepository extends JpaRepository<CovidAlarm, String> {
+
+}
