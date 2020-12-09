4dae63114cb6a875c0805e331666207d0091bc24
narare0551 <naraekang20@gmail.com>
2020-12-09 21:48:34 +0900

    covid route

--- src/main/java/com/project/MatchingPro/controller/CovidRouteController.java
+++ src/main/java/com/project/MatchingPro/controller/CovidRouteController.java
@@ -5,28 +5,28 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.MatchingPro.domain.CovidAlarm.CovidAlarm;
-import com.project.MatchingPro.domain.CovidAlarm.CovidAlarmRepository;
-
+import com.project.MatchingPro.domain.CovidRoute.CovidRoute;
+import com.project.MatchingPro.domain.CovidRoute.CovidRouteRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class CovidAlarmController {
+public class CovidRouteController {
 
 	private final HttpSession session;
-	private final CovidAlarmRepository CovidAlarmService;
-	private final CovidAlarmRepository CovidAlarmRepository;
+	private final CovidRouteRepository CovidRouteService;
+	private final CovidRouteRepository CovidRouteRepository;
 	
-	@GetMapping("/CovidAlarm")
-	public List<CovidAlarm> allData() {
-		return CovidAlarmRepository.findAll();
+	@GetMapping("/CovidRoute")
+	public List<CovidRoute> allData() {
+		return CovidRouteRepository.findAll();
 	}
 	
 }
